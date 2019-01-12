c3d13373adcb166fc5962f1cdd4a40f363a6b44e
Min Yeong Choi <mchoi00@stuy.edu>
2019-01-12 00:31:16 -0500

    the resize method almost works, just need to fix the placement of the sides

--- Display.java
+++ Display.java
@@ -33,16 +33,19 @@ public class Display {
 		*@param cube is the Cube that the method is displaying a sticker of
 		*
     */
-	private static void drawSticker(int side, int index, int x, int y,Screen screen,Cube cube) {
-		int storeY = y;
-		for (int i = 0;i < 2;i++) {
-			for (int j = 0; j < 4;j++) {
-				screen.setCharacter(x+j,storeY,cube.drawColor(side,index));
+		private static void drawSticker(int side, int index, int x, int y, int[] size, Screen screen,Cube cube) {
+			int storeY = y;
+			int length = size[0];
+			int height = size[1];
+			for (int i = 0; i < height;i++) {
+				for (int j = 0; j < length;j++) {
+					screen.setCharacter(x+j,storeY,cube.drawColor(side,index));
+				}
+				storeY++;
 			}
-			storeY++;
+
 		}
 
-	}
 
 	/**Displays an entire side of the Rubik's Cube, made up of a 3x3 grid of
 		*stickers and their corresponding colors.
@@ -54,17 +57,20 @@ public class Display {
 		*@param cube is the Cube that the method is displaying a side of
 		*
     */
-	private static void drawSide(int side,int startX, int startY,Screen screen,Cube cube) {
-		int storeStartY = startY;
-		int currentSticker = 0;
-		for (int i = 0;i < 3;i++) {
-			for (int j = 0;j < 3;j++) {
-				drawSticker(side,currentSticker,startX+(4*j),storeStartY,screen,cube);
-				currentSticker++;
+		private static void drawSide(int side,int startX, int startY,int[] size, Screen screen,Cube cube) {
+			int storeStartY = startY;
+			int currentSticker = 0;
+			int gapX = size[0];
+			int gapY = size[1];
+			for (int i = 0;i < 3;i++) {
+				for (int j = 0;j < 3;j++) {
+					drawSticker(side,currentSticker,startX+(gapX*j),storeStartY,size, screen, cube);
+					currentSticker++;
+				}
+				storeStartY += gapY;
 			}
-			storeStartY += 2;
 		}
-	}
+
 
 	/**Displays the entire Rubik's Cube, made up of 6 sides and laid out like
 		*a sideways cross.
@@ -77,14 +83,19 @@ public class Display {
 		*@param cube is the Cube that the method is displaying
 		*
 		*/
-	public static void drawCube(int startX, int startY,Screen screen, Cube cube){
-		drawSide(3,startX,startY,screen,cube);
-		drawSide(4,startX-12,startY+6,screen,cube);
-		drawSide(0,startX,startY+6,screen,cube);
-		drawSide(2,startX+12,startY+6,screen,cube);
-		drawSide(5,startX+24,startY+6,screen,cube);
-		drawSide(1,startX,startY+12,screen,cube);
-	}
+		public static void drawCube(Screen screen, Cube cube,int[] size,int[] startingPositions){
+			int length = size[0];
+			int height = size[1];
+			int startX = startingPositions[0];
+	int startY = startingPositions[1];
+			drawSide(3,startX,startY, size,screen,cube);
+			drawSide(4,startX-3*length,startY+3*height,size,screen,cube);
+			drawSide(0,startX,startY+6,size,screen,cube);
+			drawSide(2,startX-3*length,startY+3*height,size,screen,cube);
+			drawSide(5,startX-6*length,startY+3*height,size,screen,cube);
+			drawSide(1,startX,startY+6*height,size,screen,cube);
+		}
+
 
 	public static void changeCube(KeyStroke key, Screen screen, Cube cube){
 		String keyString = ""+key;
@@ -92,66 +103,58 @@ public class Display {
 		cube.performMove(toMove);
 	}
 
-	private static int[] giveSize(TerminalSize dimensions) {
-		int row = dimensions.getRows();
-		int col = dimensions.getColumns();
-		int[] sizes = new int[] {1,1};
-		if (row < 36) {
-			sizes[0] = 1;
-			sizes[1] = 1;
-	 	}
-		else if (row >= 36 && row < 72) {
-			sizes[0] = 2;
-			sizes[1] = 1;
-		}
-		else if (row >= 72 && row < 108) {
-			sizes[0] = 4;
-			sizes[1] = 2;
-		}
-		else if (row >= 108 && row < 144) {
-			sizes[0] = 6;
-			sizes[1] = 3;
-		}
-		else if (row >= 144 && row < 162) {
-			sizes[0] = 8;
-			sizes[1] = 4;
-		}
-		else if (row >= 162 && row < 198) {
-			sizes[0] = 9;
-			sizes[1] = 5;
-		}
-		else if (row >= 198 && row < 234) {
-			sizes[0] = 11;
-			sizes[1] = 6;
-		}
-		else if (row >= 234) {
-			sizes[0] = 13;
-			sizes[1] = 7;
-		}
-		return sizes;
+	private static int[] getSize(Screen screen) {
+TerminalSize dimensions = screen.getTerminalSize();
+int row = dimensions.getRows();
+	int col = dimensions.getColumns();
+	int[] sizes = new int[] {1,1};
+	if (col < 36) {
+sizes[0] = 1;
+sizes[1] = 1;
+ 	}
+else if (col >= 36 && col < 72) {
+		sizes[0] = 2;
+sizes[1] = 1;
 	}
+	else if (col >= 72 && col < 108) {
+		sizes[0] = 4;
+sizes[1] = 2;
+	}
+	else if (col >= 108 && col < 144) {
+		sizes[0] = 6;
+sizes[1] = 3;
+	}
+	else if (col >= 144 && col < 162) {
+		sizes[0] = 8;
+sizes[1] = 4;
+	}
+	else if (col >= 162 && col < 198) {
+		sizes[0] = 9;
+sizes[1] = 5;
+	}
+	else if (col >= 198 && col < 234) {
+		sizes[0] = 11;
+sizes[1] = 6;
+	}
+	else if (col >= 234) {
+		sizes[0] = 13;
+sizes[1] = 7;
+	}
+	return sizes;
+}
 
-	public static int[] getStartingPositions(TerminalSize dimensions, Screen screen, int[] sizes){
-		int row = dimensions.getRows();
-		int col = dimensions.getColumns();
-		int[] positions = new int[2];		
-		int sizeOfRLSpace = (col - (3*sizes[0]*4)) / 2;
-		int sizeOfUDSpace = (row - (3*sizes[1]*3)) / 2;
-		positions[0] = 3*sizes[0] + sizeOfRLSpace;
-		positions[1] = sizeOfUDSpace;
-		return positions;
-	}
 
-	public static void resizeCube(TerminalSize dimensions,Screen screen){
-		int row = dimensions.getRows();
-		int col = dimensions.getColumns();
-		String errorMessage = "Resize screen";
-		if (row < 12 || col < 9) {
-			for (int i = 0; i < errorMessage.length(); ++i) {
-				screen.setCharacter(0+i,4,new TextCharacter(errorMessage.charAt(i)));
-			}
-		}
-	}
+public static int[] getStartingPositions(Screen screen, int[] sizes){
+	TerminalSize dimensions = screen.getTerminalSize();
+int row = dimensions.getRows();
+int col = dimensions.getColumns();
+int[] positions = new int[2];
+int sizeOfRLSpace = (col - (3*sizes[0]*4)) / 2;
+	int sizeOfUDSpace = (row - (3*sizes[1]*3)) / 2;
+	positions[0] = 3*sizes[0] + sizeOfRLSpace;
+	positions[1] = sizeOfUDSpace;
+	return positions;
+}
 
 	/**Creates an interactive Screen that allows the user to interact with a
 		*simulation of a Rubik's Cube. The user may turn different sides,
@@ -179,7 +182,7 @@ public class Display {
 		Cube cube = new Cube();
 		String scramble = cube.generateScramble(20);
 		cube.performMoveSet(scramble);
-		drawCube(25,5,screen,cube);
+		drawCube(screen,cube,getSize(screen), getStartingPositions((screen),getSize(screen)));
 		putString(2,0,screen,scramble);
 		TerminalSize originalSize = screen.getTerminalSize();
 		putString(0,1,screen,""+originalSize);
@@ -188,15 +191,17 @@ public class Display {
 			TerminalSize currentSize = screen.getTerminalSize();
 
 			if (currentSize != originalSize) {
-				resizeCube(currentSize,screen);
+				screen.clear();
+				drawCube(screen, cube, getSize(screen), getStartingPositions((screen),getSize(screen)));
 				putString(1,0,screen,""+currentSize);
+				originalSize = currentSize;
 			}
 
 			if (key != null) {
 				if (key.getKeyType() == KeyType.Escape) break;
 				else if (key.getKeyType() == KeyType.Character) {
 					changeCube(key,screen,cube);
-					drawCube(25,5,screen,cube);
+					drawCube(screen,cube,getSize(screen), getStartingPositions((screen),getSize(screen)));
 				}
 			}
 			long tEnd = System.currentTimeMillis();
